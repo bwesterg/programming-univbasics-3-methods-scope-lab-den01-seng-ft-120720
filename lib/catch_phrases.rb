228027dd15393f1a9e78c95051eee43d83c51a8a
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-15 19:39:07 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,3 @@
-    def mario
-      phrase = "It's-a me, Mario!"
-      status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-      puts phrase
+    def toadstool
+      puts status
     end
